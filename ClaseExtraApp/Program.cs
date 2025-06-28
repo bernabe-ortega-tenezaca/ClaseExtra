dde1707ab88828ed8332f7f89a23e5cf8e2f4a69
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-27 20:28:35 -0500

    Se comenta código introductorio

--- ClaseExtraApp/Program.cs
+++ ClaseExtraApp/Program.cs
@@ -1,19 +1,19 @@
 ﻿Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine();
 
-//Pruebas de mensajes en consola
+// //Pruebas de mensajes en consola
 
-string nombre = "Carlos"; //inicializo la variable nombre
-string materia = "Quimica";
+// string nombre = "Carlos"; //inicializo la variable nombre
+// string materia = "Quimica";
 
-//Concatenar mensajes
-Console.WriteLine("Hola " + nombre + " bienvenido.");
-Console.WriteLine("Hola {0} bienvenido {1}", nombre, "a la programación");
-Console.WriteLine("Hola {0} bienvenido a {1}", nombre, materia);
-Console.WriteLine($"Hola {nombre} bienvenido a {materia}!");
+// //Concatenar mensajes
+// Console.WriteLine("Hola " + nombre + " bienvenido.");
+// Console.WriteLine("Hola {0} bienvenido {1}", nombre, "a la programación");
+// Console.WriteLine("Hola {0} bienvenido a {1}", nombre, materia);
+// Console.WriteLine($"Hola {nombre} bienvenido a {materia}!");
 
-//Lecturas desde teclado
-string ciudad;
-System.Console.WriteLine("Ingrese una ciudad");
-ciudad = Console.ReadLine();
-System.Console.WriteLine("La ciudad es: " + ciudad);
+// //Lecturas desde teclado
+// string ciudad;
+// System.Console.WriteLine("Ingrese una ciudad");
+// ciudad = Console.ReadLine();
+// System.Console.WriteLine("La ciudad es: " + ciudad);
