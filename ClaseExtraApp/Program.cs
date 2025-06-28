e67abcf4a745191d3aba2212441ac3cedf461b5f
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-06-27 20:13:39 -0500

    Concatenar cadenas con parametros

--- ClaseExtraApp/Program.cs
+++ ClaseExtraApp/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine();
 
 //Pruebas de mensajes en consola
 
 string nombre = "Carlos"; //inicializo la variable nombre
+string materia = "Quimica";
 
-Console.WriteLine("Hola "+nombre+" bienvenido.");//Concatenar mensajes
+//Concatenar mensajes
+Console.WriteLine("Hola " + nombre + " bienvenido.");
+Console.WriteLine("Hola {0} bienvenido {1}", nombre, "a la programación");
+Console.WriteLine("Hola {0} bienvenido a {1}", nombre, materia);
